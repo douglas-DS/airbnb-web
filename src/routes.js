5c852809282ca1de9376b1f006de9d1d5e2ed161
Douglas Souza <douglas_souza03@hotmail.com>
2019-09-01 00:49:22 -0300

    Initial project structure

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({ component: Component, ...rest  }) => (
+    <Route
+        {...rest}
+        render = {props => 
+            isAuthenticated() ? (
+                <Component {...props} />
+            )
+        }
+    />
+);
