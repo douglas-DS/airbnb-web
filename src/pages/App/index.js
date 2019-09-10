b8fbea8e00338fa6d74a2d80e9546fe5f49a8869
Douglas Souza <douglas_souza03@hotmail.com>
2019-09-10 14:24:31 -0300

    Component class refactored

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,42 +1,88 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Dimensions from "react-dimensions";
 import { Container } from "./styles";
 import MapGL from "react-map-gl";
 import PropTypes from "prop-types";
 
-const Map = () => {    
+import debounce from 'lodash/debounce';
+import api from '../../services/api';
 
-    const [token] = useState(
-        'pk.eyJ1IjoiZG91Z2xhcy1kcyIsImEiOiJjazA2dnVxaGQwZXpoM2lxYmIxc2VqZGdtIn0.XQ5faRbBWkYmLjJaNdF-6w'
-    );
-    const [containerWidth] = useState(PropTypes.number.isRequired);
-    const [containerHeight] = useState(PropTypes.number.isRequired);
+import Properties from './components/Properties/index';
 
-    const [viewport, setViewport] = useState({
-        latitude: -27.2108001,
-        longitude: -49.6446024,
-        zoom: 12.8,
-        bearing: 0,
-        pitch: 0   
-    });
+const TOKEN =
+  "pk.eyJ1IjoiZG91Z2xhcy1kcyIsImEiOiJjazA2dnVxaGQwZXpoM2lxYmIxc2VqZGdtIn0.XQ5faRbBWkYmLjJaNdF-6w";
 
-    return (
+class Map extends Component {
+
+    constructor() {
+        super();
+        this.updatePropertiesLocalization = debounce(
+            this.updatePropertiesLocalization, 
+            500
+        );
+    }
+
+  static propTypes = {
+    containerWidth: PropTypes.number.isRequired,
+    containerHeight: PropTypes.number.isRequired
+  };
+
+  state = {
+    viewport: {
+      latitude: -27.2108001,
+      longitude: -49.6446024,
+      zoom: 12.8,
+      bearing: 0,
+      pitch: 0
+    },
+    properties: []
+  };
+
+  componentDidMount() {
+      this.loadProperties();
+  }
+
+  updatePropertiesLocalization() {
+      this.loadProperties();
+  }
+
+  loadProperties = async () => {
+      const { latitude, longitude } = this.state.viewport;
+      try {
+          const response = await api.get('/properties', {
+              params: { latitude, longitude }
+          });
+          this.setState({ properties: response.data });
+      } catch(err) {
+          console.log(err);
+      }
+  };
+
+  render() {
+      const { containerWidth: width, containerHeight: height } = this.props;
+      const { properties } = this.state;
+      return (
         <MapGL
-            width={containerWidth}
-            height={containerHeight}
-            {...viewport}
-            mapStyle='mapbox://styles/mapbox/dark-v9'
-            mapboxApiAccessToken={token}
-            onViewportChange={viewport => setViewport({ viewport })}
-        />
-    )
+        width={width}
+        height={height}
+        {...this.state.viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+        onViewStateChange={this.updatePropertiesLocalization.bind(this)}
+        >
+        <Properties properties={properties} />
+        </MapGL>
+    );
+  }
+
 }
 
 const DimensionedMap = Dimensions()(Map);
 const App = () => (
-    <Container>
-        <DimensionedMap />
-    </Container>
+  <Container>
+    <DimensionedMap />
+  </Container>
 );
 
 export default App;
